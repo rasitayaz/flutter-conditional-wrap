2d15b3fd47ad29faa6e6345f232d9262d8dcaaea
rasitayaz <mrasitayaz@gmail.com>
2022-07-29 10:36:04 +0300

    Add implementation

--- lib/conditional_wrap.dart
+++ lib/conditional_wrap.dart
@@ -1,7 +1,41 @@
 library conditional_wrap;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/widgets.dart';
+
+typedef ParentBuilder = Widget Function(BuildContext context, Widget child);
+
+class ConditionalWrap extends StatelessWidget {
+  /// Creates a widget that conditionally wraps its [child].
+  const ConditionalWrap({
+    Key? key,
+    required this.condition,
+    required this.positiveBuilder,
+    this.negativeBuilder,
+    required this.child,
+  }) : super(key: key);
+
+  /// Decides on which [ParentBuilder] to use.
+  final bool condition;
+
+  /// Parent builder to use when [condition] is true.
+  final ParentBuilder positiveBuilder;
+
+  /// Parent builder to use when [condition] is false.
+  ///
+  /// If not specified, the [child] is returned.
+  final ParentBuilder? negativeBuilder;
+
+  /// The widget to be conditionally wrapped.
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    if (condition) {
+      return positiveBuilder(context, child);
+    } else if (negativeBuilder != null) {
+      return negativeBuilder!(context, child);
+    } else {
+      return child;
+    }
+  }
 }
