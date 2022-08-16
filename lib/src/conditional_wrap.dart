2f9e25fb42198337aa1ffecb95aabb092b7a6c7d
rasitayaz <mrasitayaz@gmail.com>
2022-08-17 00:25:31 +0300

    update conditional wrap

---
+++ lib/src/conditional_wrap.dart
@@ -0,0 +1,38 @@
+import 'package:conditional_wrap/src/builders.dart';
+import 'package:flutter/widgets.dart';
+
+class ConditionalWrap extends StatelessWidget {
+  /// Creates a widget that conditionally wraps its [child].
+  const ConditionalWrap({
+    Key? key,
+    required this.condition,
+    required this.builder,
+    this.fallback,
+    required this.child,
+  }) : super(key: key);
+
+  /// Decides on which [ParentBuilder] to use.
+  final bool condition;
+
+  /// Parent builder to use when [condition] is true.
+  final ParentBuilder builder;
+
+  /// Parent builder to use when [condition] is false.
+  ///
+  /// If not specified, the [child] is returned.
+  final ParentBuilder? fallback;
+
+  /// The widget to be conditionally wrapped.
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    if (condition) {
+      return builder(child);
+    } else if (fallback != null) {
+      return fallback!(child);
+    } else {
+      return child;
+    }
+  }
+}
